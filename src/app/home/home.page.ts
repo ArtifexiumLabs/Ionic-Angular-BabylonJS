d84e8f923273d21b2a99f854888dcf17975f4570
Artifex <atorres1929@gmail.com>
2022-03-18 12:01:39 -0400

    BabylonJS successfully added to template

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChildren, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import * as BABYLON from '@babylonjs/core';
 
 @Component({
@@ -6,40 +6,49 @@ import * as BABYLON from '@babylonjs/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements AfterViewInit {
+export class HomePage implements OnInit {
+  @ViewChild('renderCanvas', { static: true })
+  renderCanvas: ElementRef<HTMLCanvasElement>;
 
-  @ViewChildren('renderCanvas')
-  private renderCanvas: HTMLCanvasElement;
+  constructor() {}
 
-  constructor() { }
-
-  ngAfterViewInit(): void {
-      this.render();
+  ngOnInit(): void {
+    this.render();
   }
 
   render() {
-    const engine = new BABYLON.Engine(this.renderCanvas, true); // Generate the BABYLON 3D engine
+    const engine = new BABYLON.Engine(this.renderCanvas.nativeElement, true); // Generate the BABYLON 3D engine
 
     // Add your code here matching the playground format
-    const createScene = () => {
-      const scene = new BABYLON.Scene(engine);
-  
-      const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
-      camera.attachControl(this.renderCanvas, true);
-  
-      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-  
-      const box = BABYLON.MeshBuilder.CreateBox("box", {});
-  
-      return scene;
-  }
 
-    const scene = createScene(); //Call the createScene function
+    const scene = this.createScene(engine); //Call the createScene function
 
     // Register a render loop to repeatedly render the scene
-    engine.runRenderLoop(function () {
+    engine.runRenderLoop(() => {
       scene.render();
     });
   }
 
+  createScene(engine: BABYLON.Engine) {
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera(
+      'camera',
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      3,
+      new BABYLON.Vector3(0, 0, 0)
+    );
+    camera.attachControl(this.renderCanvas, true);
+
+    const light = new BABYLON.HemisphericLight(
+      'light',
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+
+    const box = BABYLON.MeshBuilder.CreateBox('box', {});
+
+    return scene;
+  }
 }
